2466ad2d09c0e87b58369d2fbb8858824092a356
Codes and Tags <codesandtags@gmail.com>
2015-06-20 08:52:21 -0500

    Create the Tic-Tac-Toe Game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("codesandtags",  []);
+(function(){
+    var app = angular.module("codesandtags",  []);
+
+    app.controller('userController', function(){
+        this.user = user;
+    });
+
+    var user ={
+        name : "Edwin Torres",
+        rol : "Creative Developer",
+        salary : 450000
+    }
+})();
+
+
